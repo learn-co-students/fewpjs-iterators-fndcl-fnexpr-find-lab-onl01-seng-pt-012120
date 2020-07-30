488f50718aa4b7f3d4f40cc65f40de8511018d4b
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-30 17:18:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(o){
+  let toReturn = (o.find( ({ result }) => result === "W"));
+  if(toReturn){
+  return toReturn["year"];
+}
 }
