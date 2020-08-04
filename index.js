18739779d3d8d41c501694d43d443ea49b8aa027
haileybongo <haileyebongo@gmail.com>
2020-08-04 14:20:55 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+    let winObj =  array.find( function(s) { return s.result=== "W"})
+    if (winObj === undefined){
+      return winObj
+    } else {return winObj.year}
+  }
+
