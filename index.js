299ab71547dd80eb760b1efc5d2f9b366ae52358
hornadam8 <hornadam8@gmail.com>
2020-08-08 10:16:13 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(e)
+{
+  let answer = undefined;
+  e.find(function(s){ if (s.result === "W"){ answer = s.year}});
+  return answer
+}
