cbca2db36805a6ce60d3308131f7af7b3ba7fd40
LeonorPDX <leonoracolbert@gmail.com>
2020-08-08 11:38:30 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let returnVal;
+  arr.find( function(obj) {
+    if (obj.result === "W") {
+      returnVal = obj.year
+    }
+  })
+  return returnVal
+}
