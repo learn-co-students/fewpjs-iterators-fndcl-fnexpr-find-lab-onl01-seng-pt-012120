b1dd991ad4a2cae18e92c8ac148978cd9636c10f
vanessuniq <vanessuniq@gmail.com>
2020-08-28 14:02:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const testVar = {}
+let testVar = {}
 
-function testFunc() {
-  return "hi"
+function testFunc(obj) {
+  return obj.result === 'W';
+};
+
+function superbowlWin(array) {
+  testVar = array.find(testFunc);
+  if (testVar) {
+    return testVar.year;
+  };
+  
 }
